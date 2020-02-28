c11beabc79276c4041af2b597aedf0a77165fd6b
Sxy623 <191436989@qq.com>
2020-02-28 23:30:13 +0800

    Firebase authentication

---
+++ Flash Chat/Flash Chat/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  Flash Chat
+//
+//  Created by 沈心逸 on 2020/2/28.
+//  Copyright © 2020 Xinyi Shen. All rights reserved.
+//
+
+struct K {
+    static let appName = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
--- Flash Chat/Flash Chat/Control/ChatViewController.swift
+++ Flash Chat/Flash Chat/Control/ChatViewController.swift
@@ -7,18 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        navigationItem.title = K.appName
+        navigationItem.hidesBackButton = true
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func logOutPresses(_ sender: UIBarButtonItem) {
+
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
     }
     
 }
