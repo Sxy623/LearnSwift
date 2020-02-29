e9ed993d378fc1e6b3e55363c1c2a90bc9059f87
Sxy623 <191436989@qq.com>
2020-02-29 22:11:24 +0800

    Swift package manager

--- Flash Chat/Flash Chat/AppDelegate.swift
+++ Flash Chat/Flash Chat/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let db = Firestore.firestore()
         print(db)
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         
         return true
     }
